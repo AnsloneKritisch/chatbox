4d930d5774b19bc73761275869fd864221bdb8e6
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2023-09-22 20:16:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/', (req, res) => {
 
     var name = req.body.name ;
 
-    if (name === "palku") 
+    if (name === "nidhu") 
     {
         res.sendFile(__dirname + '/chat.html')
         
